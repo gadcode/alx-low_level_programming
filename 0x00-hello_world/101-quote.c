041c414952605f8a5ce2b00409f06843a14d028f
Ikwuka Okoye <ikwukao@proton.me>
2023-09-07 22:14:25 -0400

    Fix: Debug 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,11 +4,13 @@
  * main - Entry point
  *
  * Return: quote in standard error.
+ *
  */
 int main(void)
 {
-	const char *quote = "and that piece of art is useful\"\
-			     - Dora Korpar, 2015-10-19\n";
+	char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	write(STDERR_FILENO, quote, strlen(quote));
+
 	return (1);
 }
