2bec0c3034d84891e25ed8d38019f0e398fcd4bf
Ikwuka Okoye <ikwukao@proton.me>
2023-09-09 00:25:34 -0400

    Modify files

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,13 +1,10 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-
+#include <time.h>
+#include <stdlib.h>
 /**
- * main - Entry point
+ * main - Returns random numbers (positive, negative, or zero)
  *
  * Return: Always 0 (Success)
- * @author: Ikwuka Okoye
- *
  */
 int main(void)
 {
@@ -15,14 +12,14 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
-    if (n > 0) {
-        printf("%d is positive\n", n);
-    } else if (n == 0) {
-        printf("%d is zero\n", n);
-    } else {
-        printf("%d is negative\n", n);
-    }
+
+	if (n == 0)
+		printf("%d is zero\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is positive\n", n);
 
 	return (0);
 }
+
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,3 +1,3 @@
-# 0x01-variables_if_else_while
+# 0x01-variabbles_if_else_while
 
---------------------------
+---------------------
