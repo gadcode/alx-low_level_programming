07077bc1cbef3e1629f5f710f9021e5850de1051
Ikwuka Okoye <ikwukao@proton.me>
2023-09-11 19:53:48 -0400

    Fix: Pass Betty coding standard

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,25 +2,24 @@
 
 /**
  * main - Print all possible combinations of single-digit numbers.
- * 
+ *
  * Return: Always 0 (Success)
- * 
  */
 int main(void)
 {
-    int num = 0;
-    
-    while (num <= 10)
-    {
-        putchar('0' + num); 
-        num++;
-        if (num == 10)
-        break;
-        putchar(',');
-        putchar(' ');
-    }
+	int num = 0;
 
-    putchar('\n');
+	while (num <= 10)
+	{
+		putchar('0' + num);
+		num++;
+		if (num == 10)
+			break;
+		putchar(',');
+		putchar(' ');
+	}
 
-    return(0);
+	putchar('\n');
+
+	return (0);
 }
