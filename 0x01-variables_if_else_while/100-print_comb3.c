4ef55a65ac1f1fe6b4e93f7347cf964de3ba6c64
Ikwuka Okoye <ikwukao@proton.me>
2023-09-11 20:23:34 -0400

    Add: 100-print_comb3 files

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Print all possible different combinations of two digits.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (int i = 0; i <= 9; i++)
+	{
+		for (int j = i + 1; j <= 9; j++)
+		{
+			putchar('0' + i);
+			putchar('0' + j);
+
+			if (i < 8 || j < 7)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+}
