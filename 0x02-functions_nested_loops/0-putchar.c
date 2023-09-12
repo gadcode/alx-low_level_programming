28f19301834d9e63ef2d809220af47c7ffe242da
Ikwuka Okoye <ikwukao@proton.me>
2023-09-12 12:18:24 -0400

    Add: README.md and 0-putchar.c files

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints "_putchar", followed by a new line.
+ *
+ * Author: @gadcode
+ * Date: 12/09/2023
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+# 0x02-functions_nested_loops
+
+----------------------
