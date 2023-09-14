ef0ae1fb399f6b6d3825b091bd82d6bf96e0f960
Ikwuka Okoye <ikwukao@proton.me>
2023-09-13 21:09:22 -0400

    Add: 100-times-table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_times_table - prints the n times table, starting with 0
+ * @n: The times table to print
+ *
+ * Author: @gadcode
+ * Date: 13/09/2023
+ *
+ * Return: the n times table.
+ *
+ */
+void print_times_table(int n)
+{
+	int i, j, result;
+
+	if (n > 15 || n < 0)
+	{
+		return;
+	}
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			result = i * j;
+
+			if (j == 0)
+				printf("%d", result);
+			else if (result < 10)
+				printf(", %d", result);
+			else if (result < 100)
+				printf(", %d", result);
+			else
+				printf(", %d", result);
+		}
+
+		printf("\n");
+	}
+}
