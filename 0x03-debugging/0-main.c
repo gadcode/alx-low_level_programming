40fc918dd390cc2f1d222a23479469717e9ffbe9
Ikwuka Okoye <ikwukao@proton.me>
2023-09-14 14:08:55 -0400

    Add: main.h and 0-main.c files

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Checks if a number is positive or negative
+ *
+ * Author: @gadcode
+ * Date: 14/09/2023
+ *
+ * Return: If the integer is positive or negative
+ *
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void positive_or_negative(int i);
+
+#endif /* MAIN_H */
