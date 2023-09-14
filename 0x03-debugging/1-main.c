f9447cd86e3e5a3f4147fd82c00b0903aee8583a
Ikwuka Okoye <ikwukao@proton.me>
2023-09-14 14:21:29 -0400

    Add: 1-main.c file

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - Causes an infinit loop.
+ * Return: Always 0 (Success).
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+	while (i < 10)
+	{
+		/* putchar(i); */
+	}
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
