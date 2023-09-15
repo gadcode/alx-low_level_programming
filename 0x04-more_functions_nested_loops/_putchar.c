49a7aac58da7ede4c807460819c24e64e6423691
Ikwuka Okoye <ikwukao@proton.me>
2023-09-15 17:05:10 -0400

    Add: _putchar.c file

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _putchar - writes the character 'c' to stdout
+ * @c: Character to be printed
+ *
+ * Author: @gadcode
+ * Date: 12/09/2023
+ *
+ * Return: 1, on success.
+ * -1 on error, and errno is set appropriately.
+ *
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
