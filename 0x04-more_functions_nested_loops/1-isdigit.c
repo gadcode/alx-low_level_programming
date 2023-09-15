04db658d6d8a444e42b4dd230d1373a92785576a
Ikwuka Okoye <ikwukao@proton.me>
2023-09-14 22:55:27 -0400

    Add: 1-isdigit and 2-mul.c files

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _isdigit - Checks whether a number is a digit
+* @c: The parameter
+*
+* Author: @gadcode
+* Date: 15/09/2023
+*
+* Return: 1 if digit, otherwise 0.
+*/
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1)
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* mul - Multiplies two integers
+* @a: First parameter
+* @b: Second parameter
+*
+* Author: @gadcode
+* Date: 15/09/2023
+*
+* Return: Multiple of the integers.
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
