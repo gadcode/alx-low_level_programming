ae33e12a128782ea4e41bf287db4efb3ec1fd3fe
Ikwuka Okoye <ikwukao@proton.me>
2023-09-18 22:59:18 -0400

    Modify 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,41 +1,26 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-#define TARGET_SUM 277
-#define PASSWORD_LENGTH 4
-
 /**
- * generateRandomDigit - generates a digit randomly.
- * Return: Random value modulus 10
+ * main - generates keygen
+ * Return: Always 0
  */
-
-int generatedRandomDigit()
-{
-	return rand() % 10;
-}
-
-/**
- * main - Entry point
- * Return: Always 0.
- */
-
 int main(void)
 {
-	srand(time(NULL));
+	int r = 0, c = 0;
+	time_t t;
 
-	int password[PASSWORD_LENGTH];
-	int currentSum = 0;
-
-	for (int i = 0; i < PASSWORD_LENGTH - 1; i++)
+	srand((unsigned int) time(&t));
+	while (c < 2772)
 	{
-		password[i] = generateRandomDigit();
-		currentSum += password[i];
+		r = rand() % 128;
+		if ((c + r) > 2772)
+			break;
+		c = c + r;
+		printf("%c", r);
 	}
-
-	password[PASSWORD_LENGTH - 1] = TARGET_SUM -currentSum;
-
-	print("%d%d%d%d\n", password[0], password[1], password[2], password[3]);
-
+	printf("%c\n", (2772 - c));
 	return (0);
 }
