1c49bebb0b5d43ef38204799715d3a7dd219a593
Ikwuka Okoye <ikwukao@proton.me>
2023-09-18 20:01:24 -0400

    Add: README.md, 0-reset_*.c, and main.h files

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets an integer to 98.
+ * @n: The integer to print its address
+ *
+ * Author: @gadcode
+ * Date: 19/09/2023
+ *
+ * Return: 98
+ */
+
+void reset_to_98(int *n)
+{
+	n = 98;
+	return (*n);
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+# 0x05-pointers_arrays_strings
+
+-----------------
