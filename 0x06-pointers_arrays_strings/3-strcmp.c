a0b4e0060bfe3dce5230250e13a39807ded67d75
Ikwuka Okoye <ikwukao@proton.me>
2023-09-22 19:25:19 -0400

    Add: 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: The first string
+ * @s2: The second string
+ *
+ * Author: @gadcode
+ * Date: 23/09/2023
+ *
+ * Return: The difference between the strings.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0' && *s2 != '\0')
+	{
+		if (*s1 < *s2)
+		{
+			return (-1);
+		}
+		else if (*s1 > *s2)
+		{
+			return (1);
+		}
+		s1++;
+		s2++;
+	}
+
+	if (*s1 == '\0' && *s2 == '\0;)
+	{
+		return (0);
+	}
+	else if (*s1 == '\0')
+	{
+		return (-1);
+	}
+	else
+	{
+		return (1);
+	}
+}
