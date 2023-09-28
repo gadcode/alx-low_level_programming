830fa6dc0854009bb1da58ccc82fbfbc54a463a0
Ikwuka Okoye <ikwukao@proton.me>
2023-09-27 20:21:39 -0400

    Add: 2-strchr.c file

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string
+ * @c: the character to be located
+ *
+ * Author: :gadcode
+ * Date: 28/09/2023
+ *
+ * Return: a pointer when the character is found.
+ */
+
+char *_strchr(xchar *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+	}
+
+	return ('\0');
+}
