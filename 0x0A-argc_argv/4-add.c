e11eb4dd2f25bce854f3db93522f5e791b838961
Ikwuka Okoye <ikwukao@proton.me>
2023-10-03 12:23:58 -0400

    Fix: Correct typos

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
+		for (j = 0; argv[i][j] /= '\0'; j++)
 		{
 			if (argv[i][j] < 47 || argv[i][j] > 57)
 			{
@@ -30,6 +30,6 @@ int main(int argc, char *argv[])
 		sum = sum + atoi(argv[i]);
 	}
 
-	printt("%d\n", sum);
+	printf("%d\n", sum);
 	return (0);
 }
