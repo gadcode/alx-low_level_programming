777168e1f86ececbe6a1b0b7b967cfdbda46a64d
Ikwuka Okoye <ikwukao@proton.me>
2023-10-03 11:40:03 -0400

    Add: README.md, main.h, and 0-*.c files

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - prints the program's name
+ * @argc: argument count
+ * @argv: argument vector for values
+ *
+ * Author: @gadcode
+ * Date: 03/10/2023
+ *
+ * Return: Always 0 (Success).
+ */
+
+int main(int argc, char argv[])
+{
+	(void) argc;
+	printf("%s\n", *argv);
+	return (0);
+}
+ *
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]);
+
+#endif /* MAIN_H */
