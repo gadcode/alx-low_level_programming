b9222431f2cb5de583f74fb7c621c2d991a32bdf
Ikwuka Okoye <ikwukao@proton.me>
2023-10-04 07:38:05 -0400

    Add: 4-*.c and 100-*.c files

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,23 @@
+#includ "main.h"
+
+/**
+ * free_grid - frees a 2 dimensional grid previouly created
+ * at alloc_grid function
+ * @grid: multi-dimensional array of integers
+ * @height: height of the grid
+ *
+ * Author: @gadcode
+ * Date: 04/10/2023
+ *
+ * Return: Nothing.
+ */
+
+void free_grid(int **grid, int height)
+{
+	if (grid != NULL && height != 0)
+	{
+		for (; height >= 0; height--)
+			free(grid[height]);
+		free(grid);
+	}
+}
