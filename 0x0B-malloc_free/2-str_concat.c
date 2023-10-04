4bc3b629cd725ba58baf6b312d3e007b28ab0a6d
Ikwuka Okoye <ikwukao@proton.me>
2023-10-04 07:15:20 -0400

    Fix: Correct bugs

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -27,7 +27,7 @@ char *str_concat(char *s1, char *s2)
 	for (j = 0; s2[j] != '\0'; j++)
 		;
 
-	strout = malloc(sizeof(char) * (i + j) + 1));
+	strout = malloc(sizeof(char) * (i + j + 1));
 
 	if (strout == NULL)
 	{
--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,7 +13,7 @@
 
 int **alloc_grid(int width, int height)
 {
-	int gridout;
+	int **gridout;
 	int i, j;
 
 	if (width < 1 || height < 1)
