90776df9dc36b24c03c64d231372ec64f7fef538
Ikwuka Okoye <ikwukao@proton.me>
2023-10-04 07:09:12 -0400

    Add: 2-*.c and 3-*.c files

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * alloc_grid - returns a pointer to a 2 dimensional aray of integers.
+ * @width: first parameter
+ * @height: second parameter
+ *
+ * Author: @gadcode
+ * Date: 04/10/2023
+ *
+ * Return: NULL, on failure and if width or height is 0
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int gridout;
+	int i, j;
+
+	if (width < 1 || height < 1)
+		return (NULL);
+
+	gridout = malloc(height * sizeof(int *));
+	if (gridout == NULL)
+	{
+		free(gridout);
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		gridout[i] = malloc(width * sizeof(int));
+		if (gridout[i] == NULL)
+		{
+			for (i--; i >= 0; i--)
+				free(gridout[i]);
+			free(gridout);
+			return (NULL);
+		}
+	}
+
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			gridout[i][j] = 0;
+
+	return (gridout);
+}
