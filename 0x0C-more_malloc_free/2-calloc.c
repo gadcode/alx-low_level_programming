2a3d19854719e83bdc56c792865bf464d63df292
Ikwuka Okoye <ikwukao@proton.me>
2023-10-07 19:25:26 -0400

    Add: Create 2-calloc.c file

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _calloc - allocates memory for an array, using 'malloc'
+ * @nmemb: elements of array
+ * @size: bytes of the array
+ *
+ * Author: @gadcode
+ * Date: 08/10/2023
+ *
+ * Return: a pointer to the allocated memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int x, y;
+	char *array;
+
+	y = nmemb * size;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	array = malloc(y);
+
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+
+	for (x = 0; x < y; x++)
+	{
+		array[x] = 0;
+	}
+
+	return (array);
+}
