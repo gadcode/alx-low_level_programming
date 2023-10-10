38c4a50125337abde0029753979b6716db557e49
Ikwuka Okoye <ikwukao@proton.me>
2023-10-10 11:20:21 -0400

    Add: Create README.md and dog.h files

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,3 @@
+# 0x0E-structures_typedef
+
+---------------
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct with dog info
+ * @name: Frist member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: store thr different data types
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+#endif /* DOG_H */
