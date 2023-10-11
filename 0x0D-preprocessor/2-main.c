5cfecc22f32519904d044c1d288d3597b1453282
Ikwuka Okoye <ikwukao@proton.me>
2023-10-10 20:37:02 -0400

    Add: Create 1-pi.h and 2-main.c files

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file it was compiled from
+ *
+ * Author: @gadcode
+ * Date: 11/10/2023
+ *
+ * Return: the name of the file.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
