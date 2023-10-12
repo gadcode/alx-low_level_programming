b34609e210f777246342ba5c6012a41224b69af0
Ikwuka Okoye <ikwukao@proton.me>
2023-10-12 17:24:34 -0400

    Add: Create README.md and header file

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,10 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+int _putchar(char c);
+
+#endif /* VARIADIC_FUNCTIONS_H */
