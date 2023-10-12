fe1695027b24b1142f855803811e0796d42146af
Ikwuka Okoye <ikwukao@proton.me>
2023-10-12 17:32:10 -0400

    Add: Create README.md and header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,5 +6,9 @@
 #include <stdarg.h>
 
 int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *seperator, const unsigned int n, ...);
+void print_strings(const char *seperator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif /* VARIADIC_FUNCTIONS_H */
