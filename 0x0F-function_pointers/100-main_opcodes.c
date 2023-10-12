42614018a7d070aad96d8e2660721f805582a1c3
Ikwuka Okoye <ikwukao@proton.me>
2023-10-12 15:41:32 -0400

    Add: Create 100-main_opcodes.c

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function with two arguments
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Description: print opcode
+ * Return: na
+ */
+
+int main(int argc, char **argv)
+{
+	int count;
+
+	count = 0;
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if (atoi(argv[1]) < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	while (count < atoi(argv[1]))
+	{
+		printf("%02x", *((unsigned char *)main + count));
+		count++;
+		if (atoi(argv[1]) > count)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
