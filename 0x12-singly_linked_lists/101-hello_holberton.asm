2015b42ff519a1d4baaae943b5153ab84745f145
Ikwuka Okoye <ikwukao@proton.me>
2023-10-20 17:30:10 -0400

    Fix: Rewrite the Assembly program

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,34 +1,17 @@
 section .data
-    hello_msg db "Hello, Holberton",10,0 ; The string with a newline character
+	fmt db "%s", 10, 0
+	msg db "Hello, Holberton", 0
 
-section .text
-    global _start
+	section .text
+	extern printf
+	global main
 
-_start:
-    ; Load the address of the format string into rdi
-    mov rdi, hello_msg
-    ; Call printf
-    call printf
-    ; Exit the program
-    call exit
-
-printf:
-    ; Standard printf calling conventions in 64-bit Linux:
-    ; rdi = format string
-    ; [rsp] = additional arguments
-
-    ; Clear any floating-point registers to maintain FPU stack alignment
-    finit
-
-    ; Call printf
-    mov rax, 1        ; printf syscall number
-    mov rdi, 1        ; file descriptor 1 (stdout)
-    mov rax, 60       ; syscall number for sys_write
-    syscall
-    ret
-
-exit:
-    ; Exit the program (syscall number 60 for sys_exit)
-    mov rax, 60
-    xor rdi, rdi
-    syscall
+main:
+	push rbp
+	mov		rsi, msg
+	mov		rdi, fmt
+	mov		rax, 0
+	call	printf
+	pop		rbp
+	mov		rax, 0
+	re
