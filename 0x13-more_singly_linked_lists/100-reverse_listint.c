b5e4f9df38752b443977724d7c06166219af353c
Ikwuka Okoye <ikwukao@proton.me>
2023-10-25 22:04:22 -0400

    Add: Create 100-reverse_listint.c

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - Reverses a listint_t linked list.
+ * @head: Pointer to a pointer to the head of the linked list.
+ *
+ * Return: A pointer to the first node of the reversed list.
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL;
+	listint_t *current = *head;
+	listint_t *next = NULL;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+
+	*head = prev;
+	return *head;
+}
