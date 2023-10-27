7d89b69029438f116426ca987c63998e53e5903f
Ikwuka Okoye <ikwukao@proton.me>
2023-10-27 18:15:33 -0400

    Add: Create 0-binary_to_uint.c file

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts binary number to an unsigned int
+ * @b: a string
+ *
+ * Return: the converted number or 0 if b is NULL
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int length, i, add;
+
+	add = 0;
+
+	if (b != NULL)
+	{
+		for (length = 0; b[length] != '\0'; length++)
+		{
+			if (b[length] != 48 && b[length] != 49)
+			{
+				return (0);
+			}
+		}
+		for (i = 0, length--; length >= 0; length--)
+		{
+			add = add + ((b[length] - '0') << i);
+			i++;
+		}
+	}
+	else
+	{
+		return (0);
+	}
+	return (add);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,5 +5,6 @@
 #include <stdlib.h>
 
 int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
 
 #endif /* MAIN_H */
