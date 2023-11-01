850bf6d56f7dcc972eee2ac58c28b7552d0cf5e2
Ikwuka Okoye <ikwukao@proton.me>
2023-11-01 15:36:31 -0400

    Add: Create 1-*.c and 2-*.c files

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text at the end of a file
+ * @filename: name of the file
+ * @text_content: string to write to the file
+ *
+ * Return: 1 on success or -1 upon failure
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int filed, i;
+
+	if (text_content == NULL)
+	{
+		text_content = "";
+	}
+
+	for (i = 0; text_content[i] != 0; i++)
+		;
+
+	filed = open(filename, O_WRONLY | O_APPEND);
+
+	if (filed == -1)
+		return (-1);
+
+	write(filed, text_content, i);
+
+	close(filed);
+	return (1);
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+	{
+		if (filename != NULL)
+		{
+			return (1);
+		}
+		else
+		{
+			return (-1);
+		}
+	}
+}
