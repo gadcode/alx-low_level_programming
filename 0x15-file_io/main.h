d10a402bd5b21e8fe9190e871928252741de6e35
Ikwuka Okoye <ikwukao@proton.me>
2023-11-01 15:42:01 -0400

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,6 +7,10 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <string.h>
+#include <elf.h>
+#include <errno.h>
+
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
